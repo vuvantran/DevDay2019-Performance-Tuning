b194b664ad2e432604394fcaed1a9df19401322b
Oz <xad.love@gmail.com>
2019-09-27 22:32:28 +0700

    api paths

--- src/main/java/com/axonactive/devdayapp/EntryPoint.java
+++ src/main/java/com/axonactive/devdayapp/EntryPoint.java
@@ -1,5 +1,9 @@
 package com.axonactive.devdayapp;
 
+import com.axonactive.devdayapp.domain.Book;
+import com.axonactive.devdayapp.domain.Comment;
+import java.util.List;
+import java.util.ArrayList;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +15,51 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class EntryPoint {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
+    private static final String API_ROOT = "/library-core/api/books";
+
+	@RequestMapping(path = API_ROOT, method = RequestMethod.GET)
+	public List<Book> getBooks() {
+        Book book = new Book();
+        book.setId(123l);
+        book.setName("Used to be king");
+        book.setAuthor("Thomas Hamstor");
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+		return books;
+	}
+
+	@RequestMapping(path = API_ROOT + "/{bookId}", method = RequestMethod.GET)
+	public Book getBookById(@PathVariable("bookId") Long bookId) {
+        Book book = new Book();
+        book.setId(bookId);
+        book.setName("Used to be king");
+        book.setAuthor("Thomas Hamstor");
+		return book;
+	}
+
+	@RequestMapping(path = API_ROOT + "/{bookId}/comments", method = RequestMethod.GET)
+	public List<Comment> getCommentByBookId(@PathVariable("bookId") Long bookId) {
+        Comment comment = new Comment();
+        comment.setId(195l);
+        comment.setContent("Good book!");
+        List<Comment> comments = new ArrayList<>(0);
+        comments.add(comment);
+        return comments;
+	}
+
+	@RequestMapping(path = API_ROOT + "/search", method = RequestMethod.POST)
+	public List<Book> searchBook() {
+        Book book = new Book();
+        book.setId(123l);
+        book.setName("Used to be king");
+        book.setAuthor("Thomas Hamstor");
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+		return books;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntryPoint.class, args);
 	}
-
 }
 
