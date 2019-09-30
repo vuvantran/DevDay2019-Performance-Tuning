320e1d45be611c8f9761e8fef2e9bb4266fd7546
optsd <transidai1705@gmail.com>
2019-09-30 10:37:18 +0700

    Refactor

--- src/main/java/com/axonactive/devdayapp/EntryPoint.java
+++ src/main/java/com/axonactive/devdayapp/EntryPoint.java
@@ -1,14 +1,18 @@
 package com.axonactive.devdayapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.axonactive.devdayapp.domain.Book;
 import com.axonactive.devdayapp.domain.Comment;
-import java.util.List;
-import java.util.ArrayList;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 
 @RestController
 @EnableAutoConfiguration
