22948f5ccebcc7e7692ca90e6095cb6e0a48880f
Minh Tien Hoang <tien.hoang@axonactive.com>
2019-10-07 14:59:58 +0700

    Refactor search query

--- src/main/java/com/axonactive/devdayapp/repo/BookRepository.java
+++ src/main/java/com/axonactive/devdayapp/repo/BookRepository.java
@@ -18,10 +18,23 @@ public interface BookRepository extends CrudRepository<Book, Long> {
 //    List<Book> findBooksWithNameContain(String keyword);
 
 
-    String BOOK_SELECT_STATEMENT = "SELECT distinct(b.*) FROM Books b left join Book_Details d on b.id = d.book_id  WHERE ";
-    String BOOK_CONDITION = "UPPER(b.name) LIKE UPPER(?1) OR UPPER(b.author) LIKE UPPER(?1) OR UPPER(b.serial_number) LIKE UPPER(?1) OR UPPER(d.description) LIKE UPPER(?1)";
+    // Select part
+    String SELECT_STATEMENT = "SELECT distinct(b.*) FROM Books b ";
 
-    @Query(value = BOOK_SELECT_STATEMENT + BOOK_CONDITION, nativeQuery = true)
+    // Join part
+    String BOOK_LEFT_JOIN_DETAILS = " left join Book_Details d on b.id = d.book_id ";
+    String BOOK_LEFT_JOIN_TAGS = " left join Book_Tags t on d.id = t.book_detail_id ";
+    String JOINS = BOOK_LEFT_JOIN_DETAILS + BOOK_LEFT_JOIN_TAGS;
+
+    // Condition part
+    String BOOK_CONDITION = " WHERE UPPER(b.name) LIKE UPPER(?1) OR UPPER(b.author) LIKE UPPER(?1) OR UPPER(b.serial_number) LIKE UPPER(?1) ";
+    String BOOK_DETAILS_CONDITION = " OR UPPER(d.description) LIKE UPPER(?1)";
+    String BOOK_TAGS_CONDITION = " OR UPPER(t.name) LIKE UPPER(?1)";
+    String CONDITION = BOOK_CONDITION + BOOK_DETAILS_CONDITION + BOOK_TAGS_CONDITION;
+
+    String ORDER = " order by b.id";
+
+    @Query(value = SELECT_STATEMENT + JOINS + CONDITION + ORDER, nativeQuery = true)
     List<Book> findBooksWithNameContain(String keyword);
 }
 
