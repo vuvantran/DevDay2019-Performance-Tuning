47b4bda4d6d4cd7c33de09103f097af0d25c8b71
Oz <xad.love@gmail.com>
2019-10-10 22:22:24 +0700

    rating api

--- src/main/java/com/axonactive/devdayapp/EntryPoint.java
+++ src/main/java/com/axonactive/devdayapp/EntryPoint.java
@@ -16,6 +16,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +27,7 @@ import com.axonactive.devdayapp.dto.BookDto;
 import com.axonactive.devdayapp.dto.CommentDto;
 import com.axonactive.devdayapp.dto.SearchingCriteria;
 import com.axonactive.devdayapp.service.BookService;
+import com.axonactive.devdayapp.service.BookDetailService;
 import com.axonactive.devdayapp.service.CommentService;
 import com.axonactive.devdayapp.service.SearchingService;
 
@@ -35,7 +37,8 @@ import com.axonactive.devdayapp.service.SearchingService;
 @ComponentScan
 public class EntryPoint {
 	private static final Logger log = LogManager.getLogger(EntryPoint.class);
-    private static final String API_ROOT = "/library-core/api/books";
+    private static final String API_ROOT = "/library-core/api";
+
     @Autowired
     private BookService bookService;
 
@@ -45,6 +48,9 @@ public class EntryPoint {
     @Autowired
     private CommentService commentService;
 
+    @Autowired
+    private BookDetailService bookDetailService;
+
 	@RequestMapping(path = API_ROOT, method = RequestMethod.GET)
 
 	public List<BookDto> getBooks() {
@@ -52,23 +58,42 @@ public class EntryPoint {
 		return bookService.getAll();
 	}
 
-	@RequestMapping(path = API_ROOT + "/{bookId}", method = RequestMethod.GET)
+	@RequestMapping(path = API_ROOT + "/books/{bookId}", method = RequestMethod.GET)
 	public BookDto getBookById(@PathVariable("bookId") Long bookId) {
 		log.info(String.format("Finding book with bookId=%s", bookId));
 		return bookService.findById(bookId);
 
 	}
 
-	@RequestMapping(path = API_ROOT + "/{bookId}/comments", method = RequestMethod.GET)
+	@RequestMapping(path = API_ROOT + "/books/{bookId}/comments", method = RequestMethod.GET)
 	public List<CommentDto> getCommentByBookId(@PathVariable("bookId") Long bookId) {
         log.info(String.format("Searching for comments of bookId=%s", 1, bookId));
         return commentService.getCommentByBookId(bookId);
 	}
 
-	@RequestMapping(path = API_ROOT + "/search", method = RequestMethod.POST)
+	@RequestMapping(path = API_ROOT + "/books/search", method = RequestMethod.POST)
 	public List<BookDto> searchBook(@RequestBody SearchingCriteria criteria) {
 		log.info(String.format("Searching for books with criteria '%s'", criteria));
         return searchingService.search(criteria);
+	}
+
+	@RequestMapping(path = API_ROOT + "/book-details/{bookDetailId}/ratings", method = RequestMethod.POST)
+	public boolean rateABook(
+            @RequestHeader(value="Authorization") Long userId,
+            @PathVariable("bookDetailId") Long bookDetailId,
+            @RequestBody Integer point
+    ) {
+        log.info("User {} do rate {} star for the book detail with id {}", userId, point, bookDetailId);
+        return bookDetailService.rateABook(userId, bookDetailId, point);
+	}
+
+	@RequestMapping(path = API_ROOT + "/book-details/{bookDetailId}/ratings", method = RequestMethod.DELETE)
+	public boolean unrateABook(
+            @RequestHeader(value="Authorization") Long userId,
+            @PathVariable("bookDetailId") Long bookDetailId
+    ) {
+        log.info("User {} do unrate the book detail with id {}", userId, bookDetailId);
+        return bookDetailService.unrateABook(userId, bookDetailId);
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/axonactive/devdayapp/service/BookDetailService.java
+++ src/main/java/com/axonactive/devdayapp/service/BookDetailService.java
@@ -1,5 +1,10 @@
 package com.axonactive.devdayapp.service;
 
 public interface BookDetailService {
+
+    boolean rateABook(Long userId, Long bookDetailId, Integer point);
+
+    boolean unrateABook(Long userId, Long bookDetailId);
+
 }
 
