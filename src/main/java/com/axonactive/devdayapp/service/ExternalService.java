8ebae4d6fb4c7d6c581132d0591e73db15201d56
Nhan Tran <nhan.tran@axonactive.com>
2019-10-19 14:37:20 +0700

    change log message

--- src/main/java/com/axonactive/devdayapp/service/ExternalService.java
+++ src/main/java/com/axonactive/devdayapp/service/ExternalService.java
@@ -1,14 +1,12 @@
 package com.axonactive.devdayapp.service;
 
-import java.util.List;
+import com.axonactive.devdayapp.Constants;
+import com.axonactive.devdayapp.dto.BookDto;
 import java.util.Collections;
-
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONObject;
 import org.springframework.web.client.RestTemplate;
-
-import com.axonactive.devdayapp.dto.BookDto;
 
 public abstract class ExternalService {
     private static final Logger log = LogManager.getLogger(ExternalService.class);
@@ -19,10 +17,11 @@ public abstract class ExternalService {
             log.info("url: " + url);
             RestTemplate restTemplate = new RestTemplate();
             String rawResponse = restTemplate.getForObject(url, String.class);
-            log.info("raw response: " + rawResponse);
             return extractBooks(rawResponse);
         } catch(Exception e) {
-            log.error("Error occurred while connecting to " + getServiceName() + ": " + e);
+            log.error(Constants.ERROR_LOG_MSG, getServiceName(),
+                "search", 
+                String.format("keyword=%s, error=%s", keyword, e));
         }
         return Collections.emptyList();
     }
